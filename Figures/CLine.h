10857053da2299a1f4d3adbe8086807863cc886d
Happysword <45149700+Happysword@users.noreply.github.com>
2018-12-03 20:07:14 +0200

    fixed elhabal ely kan ma3mool

--- Figures/CLine.h
+++ Figures/CLine.h
@@ -10,16 +10,9 @@ private:
 	Point sPoint;
 public:
 	CLine(Point, Point, GfxInfo FigureGfxInfo);
-<<<<<<< HEAD
-	virtual void Draw(Output* pOut) ;
-	 virtual bool SelectArea(int x,int y);
-=======
 
 	virtual void Draw(Output* pOut) ;
 	 virtual bool SelectArea(int x,int y);
 
-//	virtual void Draw(Output* pOut) ;
-
->>>>>>> db46d93509c2ce146a4e9203fd87cfe2f0cc7b22
 };
 #endif
