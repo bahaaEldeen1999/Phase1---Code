8f338081eea27a803a9d199c9281d21ec18d8deb
Happysword <45149700+Happysword@users.noreply.github.com>
2018-12-09 14:34:12 +0200

    Triangle done Select perfection

--- Figures/CTriangle.cpp
+++ Figures/CTriangle.cpp
@@ -11,28 +11,34 @@ void CTriangle::Draw(Output * pOut)
 {
 	pOut->DRAWTri(corner1, corner2, corner3, FigGfxInfo, Selected);
 }
-bool CTriangle:: SelectArea(int x,int y)
+int CTriangle::crossproduct (int p1x,int p1y,int p2x,int p2y) 
 {
-	float A = area (corner1.x,corner1.y,corner2.x, corner2.y, corner3.x, corner3.y); 
-  
-      
-   float A1 = area (x, y, corner2.x, corner2.y, corner3.x, corner3.y); 
-  
-   float A2 = area (corner1.x, corner1.y, x, y, corner3.x, corner3.y); 
-  
-      
-   float A3 = area (corner1.x, corner1.x, corner2.x, corner2.y, x, y); 
-    
-    float test=abs((A1 + A2 + A3) /A);
-
-   return (test>0.9&&test<1.1); 
-	
+	return -p1x*p2y+p2x*p1y;
 
 }
-float CTriangle:: area(int x1, int y1, int x2, int y2, int x3, int y3) 
-{ 
-   return abs((x1*(y2-y3) + x2*(y3-y1)+ x3*(y1-y2))/2.0); 
-} 
+
+bool CTriangle::dotproduct(int a,int b) 
+{
+	int t=a*b;
+	if(t>=0) return true;
+	return false;
+}
+bool CTriangle::sameside(int p1x,int p1y,int p2x,int p2y,int p3x,int p3y,int ax,int ay)
+{
+	int cp1=crossproduct(p1x-p2x,-p1y+p2y,p1x-ax,-p1y+ay);
+	int cp2=crossproduct(p1x-p2x,-p1y+p2y,p1x-p3x,-p1y+p3y);
+	return dotproduct(cp1,cp2);
+}
+
+bool CTriangle::SelectArea (int ax,int ay)
+{
+
+	bool test1=sameside ( corner1.x, corner1.y, corner2.x,corner2.y, corner3.x, corner3.y, ax, ay);
+	bool test2=sameside ( corner2.x,corner2.y,corner3.x,corner3.y,corner1.x,corner1.y, ax, ay);
+	bool test3=sameside ( corner1.x, corner1.y, corner3.x, corner3.y, corner2.x, corner2.y, ax, ay);
+	return (test1&&test2&&test3);
+
+}
 void CTriangle :: save (ofstream &OutFile, int n) {
 	ID = n;
 	OutFile<<"Triangle "<<"       ";
--- Figures/CTriangle.h
+++ Figures/CTriangle.h
@@ -6,9 +6,11 @@ private:
 	Point corner1, corner2, corner3;
 public:
 	CTriangle(Point, Point, Point, GfxInfo FigureGfxInfo);
-
+	bool sameside(int p1x,int p1y,int p2x,int p2y,int p3x,int p3y,int ax,int ay);
+	bool dotproduct(int a,int b);
+	int crossproduct (int p1x,int p1y,int p2x,int p2y) ;
 	virtual void Draw(Output* pOut) ;
-	 virtual bool SelectArea(int x,int y);
+	 virtual bool SelectArea(int ax,int ay);
 	 float area(int x1, int y1, int x2, int y2, int x3, int y3) ;
 	 void save (ofstream &OutFile , int );
 	void load (ifstream &Infile ); 
