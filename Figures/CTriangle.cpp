50cbd8e0dff377ede1b512885be3cf9339f566b6
bahaaEldeen <bahaaeldeen1999@gmail.com>
2018-12-17 19:35:52 +0200

    tri

--- Figures/CTriangle.cpp
+++ Figures/CTriangle.cpp
@@ -5,6 +5,7 @@ CTriangle::CTriangle(Point P1, Point P2, Point P3, GfxInfo FigureGfxInfo):CFigur
 	corner1 = P1;
 	corner2 = P2;
 	corner3 = P3;
+	figtype=3;
 }
 
 void CTriangle::Draw(Output * pOut) 
@@ -88,6 +89,7 @@ Infile>>corner2.y;
 Infile>>corner3.x; 
 Infile>>corner3.y;
 string y;
+string fil;
 Infile>>y;
 string  Clr;
 Infile>>Clr;
@@ -111,12 +113,30 @@ if (y== "NON_FILL") {
 }
 else {
 	FigGfxInfo.isFilled = true;
+	fil = y;
+	if (fil=="BLACK") {
+		FigGfxInfo.FillClr= BLACK;
+}
+else if (fil=="RED") {
+	FigGfxInfo.FillClr = RED;
+}
+else if (fil=="BLUE") {
+	FigGfxInfo.FillClr = BLUE;
+}
+else if (fil=="GREEN") {
+	FigGfxInfo.FillClr = GREEN;
+}
+else   {
+	FigGfxInfo.FillClr = WHITE;
+}
 }
 }
 
 void CTriangle:: PrintInfo(Output* pOut)
 {
-	string p="triangle ID:   Point 1: ";
+
+	string p="Triangle ID:   Point 1: ";
+
 	p+=to_string(corner1.x)+' '+to_string(corner1.y)+"Point 2:"+to_string(corner2.x)+' '+to_string(corner2.y) +"Point 3: "+to_string(corner3.x)+' '+to_string(corner3.y);
 	pOut->PrintMessage(p);
 
